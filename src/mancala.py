15c219bbd11305d6ae8d7576cd7ee9900a5477c8
Loic Grumiaux <loic.grumiaux@student.uclouvain.be>
2023-06-14 18:28:14 -0400

    [FIX] Mancala quick fix.

--- src/mancala.py
+++ src/mancala.py
@@ -2,9 +2,10 @@ from coliseum.examples.mancala.board_mancala import BoardMancala
 from coliseum.examples.mancala.game_state_mancala import GameStateMancala
 from coliseum.examples.mancala.master_mancala import MasterMancala
 from coliseum.examples.mancala.player_mancala import PlayerMancala
+from coliseum.player.player import LocalPlayerProxy
 
-player1 = PlayerMancala(name="louis")
-player2 = PlayerMancala(name="loic")
+player1 = LocalPlayerProxy(PlayerMancala(name="louis"))
+player2 = LocalPlayerProxy(PlayerMancala(name="loic"))
 list_players = [player1, player2]
 init_scores = {player1.get_id(): 0, player2.get_id(): 0}
 init_rep = BoardMancala()
@@ -12,4 +13,4 @@ initial_game_state = GameStateMancala(init_scores, player1, list_players, init_r
 
 master = MasterMancala(name = "Mancala", initial_game_state = initial_game_state, players_iterator = list_players, log_file = "log.txt")
 
-master.play_game()
+master.record_game()
