b9746a2482ee913f4899b189778079cee8544f02
gillonlo <74982380+gillonlo@users.noreply.github.com>
2023-06-21 14:35:41 -0400

    [ADD] abalone files

---
+++ src/abalone.py
@@ -0,0 +1,48 @@
+from coliseum.examples.avalam.alpha_player_avalam import AlphaPlayerAvalam
+from coliseum.examples.avalam.board_avalam import BoardAvalam, PieceAvalam
+from coliseum.examples.avalam.game_state_avalam import GameStateAvalam
+from coliseum.examples.avalam.master_avalam import MasterAvalam
+from coliseum.examples.avalam.random_player_avalam import RandomPlayerAvalam
+from coliseum.player.player import LocalPlayerProxy
+
+
+def run_multiple_games():
+    for _ in range(1):
+        player1 = LocalPlayerProxy(RandomPlayerAvalam("R", name="louis"))
+        player2 = LocalPlayerProxy(AlphaPlayerAvalam("Y", name="loic"))
+
+        list_players = [player1, player2]
+        init_scores = {player1.get_id(): 0, player2.get_id(): 0}
+        dim = [9, 9]
+        env = {}
+        initial_board = [
+            [0, 0, 1, -1, 0, 0, 0, 0, 0],
+            [0, 1, -1, 1, -1, 0, 0, 0, 0],
+            [0, -1, 1, -1, 1, -1, 1, 0, 0],
+            [0, 1, -1, 1, -1, 1, -1, 1, -1],
+            [1, -1, 1, -1, 0, -1, 1, -1, 1],
+            [-1, 1, -1, 1, -1, 1, -1, 1, 0],
+            [0, 0, 1, -1, 1, -1, 1, -1, 0],
+            [0, 0, 0, 0, -1, 1, -1, 1, 0],
+            [0, 0, 0, 0, 0, -1, 1, 0, 0],
+        ]
+        for i in range(dim[0]):
+            for j in range(dim[1]):
+                if initial_board[i][j] == 1:
+                    env[(i, j)] = PieceAvalam(piece_type=player1.get_piece_type(), owner=player1, value=1)
+                    init_scores[player1.get_id()] += 1
+                elif initial_board[i][j] == -1:
+                    env[(i, j)] = PieceAvalam(piece_type=player2.get_piece_type(), owner=player2, value=1)
+                    init_scores[player2.get_id()] += 1
+        init_rep = BoardAvalam(env=env, dim=dim)
+        initial_game_state = GameStateAvalam(
+            scores=init_scores, next_player=player1, players=list_players, rep=init_rep
+        )
+
+        master = MasterAvalam(
+            name="Avalam", initial_game_state=initial_game_state, players_iterator=list_players, log_file="log.txt"
+        )
+        master.record_game()
+
+
+run_multiple_games()
---
+++ src/coliseum/examples/abalone/alpha_player_avalam.py
@@ -0,0 +1,79 @@
+import math
+
+from coliseum.game.action import Action
+from coliseum.game.game_state import GameState
+from coliseum.player.player import Player
+
+infinity = math.inf
+
+class AlphaPlayerAvalam(Player):
+    """
+    Attributes:
+        id_player (int): id of the player
+        name (str): name of the player
+
+    Class attributes:
+        next_id (int): id to be assigned to the next player
+    """
+
+    def __init__(self, piece_type: str, name: str = "bob") -> None:
+        super().__init__(name)
+        self.piece_type = piece_type
+
+    def get_piece_type(self):
+        """
+        Returns:
+            piece_type: string to represent the type of the piece
+        """
+        return self.piece_type
+
+    def cutoff_depth(self, d, cutoff):
+        return d > cutoff
+
+    def heuristic(self, current_state : GameState):
+        # TODO: review to make beautiful
+        players_list = current_state.get_players()
+        players_list.index(self)
+        return current_state.get_scores()[self.get_id()]
+
+    def max_value(self, current_state : GameState, alpha : int, beta : int, depth : int, cutoff : int):
+        if self.cutoff_depth(depth, cutoff) or current_state.is_done():
+            return self.heuristic(current_state), None
+        v, move = -infinity, None
+        for a in current_state.get_possible_actions():
+            v2, _ = self.min_value(a.get_new_gs(), alpha, beta, depth+1, cutoff)
+            if v2 > v:
+                v, move = v2, a
+                alpha = max(alpha, v)
+            if v >= beta:
+                return v, move
+        return v, move
+
+    def min_value(self, current_state : GameState, alpha : int, beta : int, depth : int, cutoff : int):
+
+        if self.cutoff_depth(depth, cutoff) or current_state.is_done():
+            return self.heuristic(current_state), None
+        v, move = +infinity, None
+        for a in current_state.get_possible_actions():
+            v2, _ = self.max_value(a.get_new_gs(), alpha, beta, depth+1, cutoff)
+            if v2 < v:
+                v, move = v2, a
+                beta = min(beta, v)
+            if v <= alpha:
+                return v, move
+        return v, move
+
+    def solve(self, current_state : GameState, **_) -> Action:
+        """
+        Function to implement the logic of the player (here alpha beta algorithm)
+        """
+        depth = 0
+        cutoff = 2
+        v, move = self.max_value(current_state, -infinity, +infinity, depth, cutoff)
+        #print(self.get_id(), v)
+        #v, move = self.min_value(current_state, -infinity, +infinity, depth, cutoff)
+
+        return move
+
+    def __str__(self) -> str:
+        return super().__str__()
