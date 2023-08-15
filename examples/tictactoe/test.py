a775a830cade31de0d104ca3efa769d86e059171
gillonlo <74982380+gillonlo@users.noreply.github.com>
2023-08-15 15:36:25 -0400

    [UPD] Error

--- examples/tictactoe/test.py
+++ examples/tictactoe/test.py
@@ -1,8 +1,15 @@
 import argparse
+import asyncio
+import os
 from os.path import basename, splitext
+import platform
 from board_tictac import BoardTictac
+from player_tictac import PlayerTictac
 from master_tictac import MasterTictac
 from game_state_tictac import GameStateTictac
+from seahorse.player.proxies import LocalPlayerProxy, RemotePlayerProxy
+from seahorse.utils.gui_client import GUIClient
+from seahorse.utils.recorders import StateRecorder
 
 
 if __name__=="__main__":
@@ -10,24 +17,30 @@ if __name__=="__main__":
                         prog="Launcher",
                         description="What the program does",
                         epilog="Text at the bottom of help")
-    parser.add_argument("-t","--type",required=True,type=str, choices=["local", "remote", "humvscomp", "humvshum"])
+    parser.add_argument("-t","--type",required=True,type=str, choices=["local", "remote", "standalone", "humvscomp", "humvshum"])
     parser.add_argument("-n","--nb_players",required=True,type=int)
-    parser.add_argument("-add","--address",required=False)
-    parser.add_argument("-p","--port",required=False,type=int)
-    parser.add_argument("-g","--gui",required=False,type=int)
-    parser.add_argument("-l","--log",required=False,choices=["DEBUG","INFO"])
+    parser.add_argument("-add","--address",required=False, default="localhost")
+    parser.add_argument("-p","--port",required=False,type=int, default=16001)
+    parser.add_argument("-g","--gui",required=False,type=int, default=0)
+    parser.add_argument("-gd","--gui_dist",required=False,type=int, default=0)
+    parser.add_argument("-r","--record",action="store_true",default=False)
+    parser.add_argument("-l","--log",required=False,choices=["DEBUG","INFO"], default="DEBUG")
     parser.add_argument("list_players",nargs="*")
     args=parser.parse_args()
     
     type = vars(args).get("type")
     nb_players = vars(args).get("nb_players")
-    address = vars(args).get("address") if vars(args).get("address") is not None else "localhost"
-    port = vars(args).get("port") if vars(args).get("port") is not None else 16001
-    gui = vars(args).get("gui") if vars(args).get("gui") is not None else 0
-    log_level = vars(args).get("log") if vars(args).get("log") is not None else "INFO"
+    address = vars(args).get("address")
+    port = vars(args).get("port")
+    gui = vars(args).get("gui")
+    gui_dist = vars(args).get("gui_dist")
+    record = vars(args).get("record")
+    log_level = vars(args).get("log")
     list_players = vars(args).get("list_players")
     
     print(type, nb_players, address, port, gui, list_players)
+    
+    gui_path =  "./GUI/index.html"
     
     if type == "local" :
         player1_class = __import__(splitext(basename(list_players[0]))[0], fromlist=[None])
@@ -40,11 +53,33 @@ if __name__=="__main__":
         initial_game_state = GameStateTictac(
             scores=init_scores, next_player=player1, players=list_players, rep=init_rep)
         master = MasterTictac(
-            name="Tic-Tac-Toe", initial_game_state=initial_game_state, players_iterator=list_players, log_level=log_level, port=port, n_listeners=gui,
+            name="Tic-Tac-Toe", initial_game_state=initial_game_state, players_iterator=list_players, log_level=log_level, port=port,
         )
-        master.record_game()
+        listeners = [GUIClient(path=gui_path)]*gui+[GUIClient()]*gui_dist
+        if record :
+            listeners.append(StateRecorder())
+        master.record_game(listeners=listeners)
     elif type == "remote" :
+        player1_class = __import__(splitext(basename(list_players[0]))[0], fromlist=[None])
+        player1 = LocalPlayerProxy(player1_class.MyPlayer("X", name=splitext(basename(list_players[0]))[0]),gs=GameStateTictac)
+        player2 = RemotePlayerProxy(mimics=PlayerTictac,piece_type="O",name=splitext(basename(list_players[1]))[0])
+        list_players = [player1, player2]
+        init_scores = {player1.get_id(): 0, player2.get_id(): 0}
+        init_rep = BoardTictac(env={}, dim=[3, 3])
+        initial_game_state = GameStateTictac(
+            scores=init_scores, next_player=player1, players=list_players, rep=init_rep)
+        master = MasterTictac(
+            name="Tic-Tac-Toe", initial_game_state=initial_game_state, players_iterator=list_players, log_level=log_level, port=port,
+        )
+        listeners = [GUIClient(path=gui_path)]*gui+[GUIClient()]*gui_dist
+        if record :
+            listeners.append(StateRecorder())
+        master.record_game(listeners=listeners)
         print("ok2")
+    elif type == "standalone" :
+        player2_class = __import__(splitext(basename(list_players[0]))[0], fromlist=[None])
+        player2 = LocalPlayerProxy(player2_class.MyPlayer("O", name=splitext(basename(list_players[0]))[0]),gs=GameStateTictac)
+        asyncio.new_event_loop().run_until_complete(player2.listen(keep_alive=True,master_address=f"http://{address}:{port}"))
     elif type == "humvscomp" :
         print("ok3")
     elif type == "humvshum" :
