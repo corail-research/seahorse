d3871b1537eff3848b259d63c9219e04c5a73af7
Loic Grumiaux <loic.grumiaux@student.uclouvain.be>
2023-08-28 11:48:13 -0400

    [DEL] Legacy attribute test.

--- tests/utils/test_timer.py
+++ tests/utils/test_timer.py
@@ -40,7 +40,6 @@ class MixinTestCase(unittest.TestCase):
         self.dummy.init_timer(10)
         assert self.dummy.get_time_limit() == 10
         assert self.dummy.get_remaining_time() == 10
-        assert self.dummy._timer_initialized
         assert not self.dummy.is_locked()
         assert not self.dummy.is_running()
 
